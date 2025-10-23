9a02a7f930ee284986e23d4e63c19caadb9e88a1
adelisa27 <adelisapalavrtic27@gmail.com>
2025-10-23 17:06:08 +0200

    Add files via upload

---
+++ VjezbeV/Game.java
@@ -0,0 +1,36 @@
+package projekat2';
+
+public class Game {
+
+	
+		
+		import java.util.ArrayList;
+		
+
+			public static void main(String[] args) {
+				ArrayList<Player> igraci = new ArrayList<>();
+				Player player1 = new Player("player1",10,5,32,32,85);
+				igraci.add(player1);
+				
+				ArrayList<Enemy> protivnik = new ArrayList<>();
+				Enemy Goblin = new Enemy("Golbin",12,5,16,15,20);
+				protivnik.add(Goblin);
+				
+				
+				
+				
+				
+				
+				System.out.println(igraci.get(0));
+				System.out.println(protivnik.get(0));
+				
+				
+				
+       
+		 }
+			
+  
+
+	
+
+
